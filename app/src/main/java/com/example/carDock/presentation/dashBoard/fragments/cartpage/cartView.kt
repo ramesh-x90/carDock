dd08a8838ac7dd42cc0e805e5d5bae5d40f07578
RAMESH <rameshshyaman@gmail.com>
2022-04-09 21:51:01 +0530

    building car detail page

---
+++ app/src/main/java/com/example/carDock/presentation/dashBoard/fragments/cartpage/cartView.kt
@@ -0,0 +1,11 @@
+package com.example.carDock.presentation.dashBoard.fragments.cartpage
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun CartPage(navController: NavHostController) {
+
+}
+
